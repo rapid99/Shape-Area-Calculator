af07f3a223755c2c53d70b3198c1029b2bcc01ef
Matt <matthyman03@gmail.com>
2016-06-28 18:58:06 -0600

    Update AreaCalculator.py

--- AreaCalculator.py
+++ AreaCalculator.py
@@ -20,11 +20,11 @@ hint = "Don't forget to include the correct unit"
 option = raw_input("Enter C for Circle or T for Triangle").upper()
 
 if option == 'C':
-    radius = float(raw_input("Enter radius: "))
-    area = pi * radius**2  
-    print "The pie is baking..."
-    sleep(1) 
-    print ("Area: %.2f. \n%s" % (area, hint))
+  radius = float(raw_input("Enter radius: "))
+  area = pi * radius**2  
+  print "The pie is baking..."
+  sleep(1) 
+  print ("Area: %.2f. \n%s" % (area, hint))
 elif option == "T":
   base = float(raw_input("Enter base: "))
   height = float(raw_input("Enter height: "))
